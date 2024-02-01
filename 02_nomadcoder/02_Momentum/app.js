a27a9572f4e542c2f3faa01a06c3e852d06dc03a
sunny <sunnya2023@gmail.com>
2024-02-01 22:27:04 +0900

    login

--- 02_nomadcoder/02_Momentum/app.js
+++ 02_nomadcoder/02_Momentum/app.js
@@ -1,11 +1,17 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+const link = document.querySelector("a");
 
+const HEDDEN_CLASSNME = "hidden";
 function onLoginSubmit(e) {
   // console.dir(loginInput)
-  e.preventDefault()
+  e.preventDefault();
+  loginForm.classList.add(HEDDEN_CLASSNME);
   const username = loginInput.value;
-  console.log(username);
+  localStorage.setItem("username", username);
+  greeting.textContent = `hello ${username}`;
+  greeting.classList.remove(HEDDEN_CLASSNME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
