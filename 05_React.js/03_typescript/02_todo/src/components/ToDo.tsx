d5fafdb67eeebb67798c2eaf1de266672024e3ed
sunny <sunnya2023@gmail.com>
2024-03-19 20:25:40 +0900

    todo button name change

--- 05_React.js/03_typescript/02_todo/src/components/ToDo.tsx
+++ 05_React.js/03_typescript/02_todo/src/components/ToDo.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import { IToDO, toDoState } from "../atoms";
 import { useSetRecoilState } from "recoil";
 
-function ToDo({ text, category }: IToDO) {
+function ToDo({ text, category, id }: IToDO) {
   const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
+    setToDos((prevToDos) => {
+      return prevToDos.map((todo) =>
+        todo.id === id ? { ...todo, category: name as any } : todo
+      );
+    });
+
+    // setToDos((oldTodo) => {
+    //   const targetIndex = oldTodo.findIndex((toDo) => toDo.id === id);
+    //   const newTodo = { text, id, category: name as IToDO["category"] };
+    //   return oldTodo;
+    // });
   };
   return (
     <li>
