27035417ce431e3a8cdbc7c2a0e5c29db8867eab
sunny <sunnya2023@gmail.com>
2024-03-23 00:31:23 +0900

    slide효과 & 커스텀

--- 05_React.js/03_typescript/04_animation/src/App.tsx
+++ 05_React.js/03_typescript/04_animation/src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import {
+  AnimatePresence,
   delay,
   motion,
   useMotionValue,
@@ -7,10 +8,10 @@ import {
   useScroll,
   useTransform,
 } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -23,8 +24,15 @@ const Box = styled(motion.div)`
   height: 200px;
   /* display: grid; */
   /* grid-template-columns: repeat(2, 1fr); */
+  position: absolute;
+  top: 100px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 const Circle = styled(motion.div)`
@@ -118,6 +126,7 @@ function App() {
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
     console.log("scrollYProgress", latest);
   });
+  //SVG효과
   const svg = {
     start: {
       pathLength: 0,
@@ -128,27 +137,79 @@ function App() {
       pathLength: 1,
     },
   };
+
+  //사라지는 효과
+  const disapear = {
+    initial: {
+      opacity: 0,
+      scale: 0,
+    },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      rotateZ: 360,
+    },
+    leaving: {
+      opacity: 0,
+      scale: 0,
+      y: 50,
+    },
+  };
+  const [showing, setShowing] = useState(false);
+  const toggleShowing = () => setShowing(!showing);
+
+  //slide
+  const slideBox = {
+    entry: (back: boolean) => ({
+      x: back ? -500 : 500,
+      opacity: 0,
+      scale: 0,
+    }),
+    center: {
+      x: 0,
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+    exit: (back: boolean) => ({
+      x: back ? 500 : -500,
+      opacity: 0,
+      scale: 0,
+      rotateX: 180,
+      transition: {
+        duration: 1,
+      },
+    }),
+  };
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
-    <Wrapper style={{ background: gradient }}>
-      {
-        <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-          <motion.path
-            variants={svg}
-            initial="start"
-            animate="end"
-            transition={{
-              default: { duration: 5 },
-              fill: { duration: 2, delay: 3 },
-            }}
-            d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z"
-          />
-        </Svg>
-      }
-      <Box
-        style={{ x, rotateZ: rotate, scale: scroll }}
-        drag="x"
-        dragSnapToOrigin
-      />
+    <Wrapper>
+      <AnimatePresence mode="wait" custom={back}>
+        <Box
+          custom={back}
+          variants={slideBox}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
+      </AnimatePresence>
+      <button onClick={nextPlease}>Next</button>
+      <button onClick={prevPlease}>prev</button>
     </Wrapper>
   );
 }
